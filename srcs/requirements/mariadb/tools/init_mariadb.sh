ee6a31a1cdb30061806b0a75eaed6267b80eebf3
Son_Hero <heroson0925@gmail.com>
2022-10-24 19:28:38 +0900

    환경변수 활용해서 코드 수정 완료

--- srcs/requirements/mariadb/tools/init_mariadb.sh
+++ srcs/requirements/mariadb/tools/init_mariadb.sh
@@ -9,7 +9,7 @@ if [ $? -ne 0 ]; then
 	mysql -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD'";
 	mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%'";
 	mysql -e "FLUSH PRIVILEGES";
-	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOTPWD'";
+	mysql -e "ALTER USER '$DB_ROOT'@'localhost' IDENTIFIED BY '$DB_ROOTPWD'";
 
 	touch /var/lib/mysql/.setup
 fi
