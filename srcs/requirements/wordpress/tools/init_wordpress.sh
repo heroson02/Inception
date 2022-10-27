1e6d09c02aa3be853c19d165e48954237e8dab5c
Son_Hero <heroson0925@gmail.com>
2022-10-27 12:30:57 +0900

    Finish 😀
    mariadb 컨테이너가 mysqld_safe에서 일어나는 에러로 재시작하는 문제 해결

--- srcs/requirements/wordpress/tools/init_wordpress.sh
+++ srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -7,9 +7,6 @@ if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
 	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbhost=$DB_HOST --allow-root
 	wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMINUSER --admin_password=$WP_ADMINPWD --admin_email=$WP_ADMINEMAIL --skip-email --allow-root && \
 	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root && \
-	# wp config create --dbname=Data --dbuser=yson --dbpass=password --dbhost=mariadb --allow-root
-	# wp core install --url=yson.42.fr --title=yson_inception --admin_user=root --admin_password=0000 --admin_email=heroson0925@gmail.com --skip-email --allow-root && \
-	# wp user create yson yson@student.42seoul.kr --role=author --user_pass=0000 --allow-root && \
 	wp plugin update --all --allow-root
 fi
 
